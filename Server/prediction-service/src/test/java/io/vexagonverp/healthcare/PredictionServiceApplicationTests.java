1229eed7eba0e31357ae9acd043abde1e3f69392
VexagonVerp <verproti80@yahoo.com.vn>
2021-11-30 23:55:33 +0700

    Service init

---
+++ Server/prediction-service/src/test/java/io/vexagonverp/healthcare/PredictionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.vexagonverp.healthcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PredictionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
